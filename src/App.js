2b885ab76307fb5edbe030039f261a3fa7a8c137
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-01-31 14:51:18 +0100

    forgot to push and submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import contacts from "./contacts.json";
+import "./App.css";
+import { useState } from "react";
+
+const five = [];
+for (let i = 0; i < 5; i++) {
+  five.push(contacts[i]);
+}
+console.log(contacts);
 
 function App() {
+  const [contact, setContact] = useState(five);
+  const addContact = () => {
+    const rand = Math.floor(Math.random() * contacts.length);
+    const newContact = contacts[rand];
+    if (five.includes(newContact)) {
+      addContact();
+    } else setContact([five, newContact]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addContact}>Add random contact</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {five.map((contact) => (
+            <tr>
+              <td>
+                {" "}
+                <img src={contact.pictureUrl} alt="oops"></img>
+              </td>
+              <td> {contact.name}</td>
+              <td> {contact.popularity}</td>
+              <td> {contact.wonOscar ? "🏆" : ""} </td>
+              <td> {contact.wonEmmy ? "🏆" : ""} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
